56a758af30722907975db918293d6d03d9766662
D57E4D <108551147+fanta512@users.noreply.github.com>
2023-05-23 07:15:36 +0400

    Convenience output

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "Enter the value of n for the nth Fibonacci number: ";
     cin >> n;
     int a = 0, b = 1, c;
     cout << "First " << n << " numbers in Fibonacci sequence: ";
