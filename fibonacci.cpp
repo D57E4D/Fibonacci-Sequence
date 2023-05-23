bc1d197f93f25b1f53ff4b3db7da6df8e79d0602
D57E4D <108551147+fanta512@users.noreply.github.com>
2023-05-23 07:11:50 +0400

    small correction

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-    int n = 10;
+    int n;
+    cin >> n;
     int a = 0, b = 1, c;
     cout << "First " << n << " numbers in Fibonacci sequence: ";
     for (int i = 0; i < n; i++)
