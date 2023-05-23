7fa9020a3a3546a3c4b0738655d03fd01208c934
David <wilsonfox655@gmail.com>
2023-05-23 06:59:15 +0400

    Added recursive Fibonacci function

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main()
 {
-    cout << "Hello, World!" << endl;
+    int n = 10;
+    cout << "Fibonacci number for n = " << n << ": ";
+    cout << fib(n) << endl;
 
     return 0;
 }
